94845881f505be55797e7e40cd0d2c5cc0db8703
unknown <daudakolo16@gmail.com>
2023-10-07 23:24:43 +0100

    intial commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import logo from "./logo.svg";
 import "./App.css";
 import { Route, Routes } from "react-router-dom";
-import RepoList from "./components/home";
+import Home from "./components/home";
 import AboutUs from "./components/about";
 
 function App() {
   return (
     <div className="App">
       <Routes>
-        <Route exact path="/" element={<RepoList />} /> {/* RepoList is the Home component */}
+        <Route exact path="/" element={<Home />} />
         <Route path="/about" element={<AboutUs />} />
       </Routes>
     </div>
